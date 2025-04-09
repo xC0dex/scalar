3d3e62a8957c8632d8636cda758087aa2b589f25
Justin Lampe <xC0dex@users.noreply.github.com>
2025-04-09 15:08:34 +0200

    Scalar.Aspire integration

--- integrations/aspnetcore/playground/Scalar.Aspire.AppHost/Program.cs
+++ integrations/aspnetcore/playground/Scalar.Aspire.AppHost/Program.cs
@@ -1,24 +1,34 @@
+using Projects;
 using Scalar.Aspire;
 using Scalar.AspNetCore;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-var bookstore = builder.AddProject<Projects.Scalar_AspNetCore_Playground>("bookstore");
+var firstBookstore = builder.AddProject<Scalar_AspNetCore_Playground>("bookstore-first");
+var secondBookstore = builder.AddProject<Scalar_AspNetCore_Playground>("bookstore-second");
 
 var scalar = builder.AddScalarApiReference(options =>
 {
     options.AutoProxy = true;
     options.CdnUrl = "https://cdn.jsdelivr.net/npm/@scalar/api-reference";
-    options.Theme = ScalarTheme.DeepSpace;
     options.Title = "Aspire API Reference";
     options
         .WithPreferredScheme("ApiKey")
         .WithApiKeyAuthentication(x => x.Token = "my-api-key");
-}).WithHttpsEndpoint(port: 54678, isProxied: false);
+}).WithHttpsEndpoint(54678, isProxied: false);
 
-scalar.WithReference(bookstore, options =>
-{
-    options.AddDocuments("v1", "v2");
-});
+scalar
+    .WithReference(firstBookstore, options =>
+    {
+        options
+            .AddDocuments("v1", "v2")
+            .WithTheme(ScalarTheme.DeepSpace);
+    })
+    .WithReference(secondBookstore, options =>
+    {
+        options
+            .AddDocuments("v1", "v2")
+            .WithTheme(ScalarTheme.Mars);
+    });
 
 builder.Build().Run();
